aeb46e16c51b0e0b9724b8737121fab57321472c
keneanung <keneanung@googlemail.com>
2022-11-16 22:37:38 +0100

    Update upload_test.ps1

--- upload_test.ps1
+++ upload_test.ps1
@@ -2,10 +2,10 @@
   # activate higher TLS version. Seems PS only uses 1.0 by default
   # credit: https://stackoverflow.com/questions/41618766/powershell-invoke-webrequest-fails-with-ssl-tls-secure-channel/48030563#48030563
   [Net.ServicePointManager]::SecurityProtocol = [System.Security.Authentication.SslProtocols] "tls, tls11, tls12"
-  (New-Object System.Net.WebClient).DownloadFile("https://downloads.sourceforge.net/project/winscp/WinSCP/5.21.5/WinSCP-5.21.5-Automation.zip?ts=gAAAAABjdU7yfz3jBCtk86Yif2GdJhhZQEMzJQye7xk1r9lh_8BxWqmPN4l9WCuLj1zhuYOmy8dRoPuDFiCV8hYCtM8a9_Y6ZA%3D%3D&r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fwinscp%2Ffiles%2FWinSCP%2F5.21.5%2FWinSCP-5.21.5-Automation.zip%2Fdownload", "C:\src\Winscp-automation.zip")
+  (New-Object System.Net.WebClient).DownloadFile("https://downloads.sourceforge.net/project/winscp/WinSCP/5.21.5/WinSCP-5.21.5-Automation.zip?ts=gAAAAABjdU7yfz3jBCtk86Yif2GdJhhZQEMzJQye7xk1r9lh_8BxWqmPN4l9WCuLj1zhuYOmy8dRoPuDFiCV8hYCtM8a9_Y6ZA%3D%3D&r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fwinscp%2Ffiles%2FWinSCP%2F5.21.5%2FWinSCP-5.21.5-Automation.zip%2Fdownload", "Winscp-automation.zip")
   Add-Type -AssemblyName System.IO.Compression.FileSystem
-  [System.IO.Compression.ZipFile]::ExtractToDirectory("C:\src\Winscp-automation.zip", "C:\src\Winscp-automation\")
-  Add-Type -Path 'C:\src\Winscp-automation\WinSCPnet.dll'
+  [System.IO.Compression.ZipFile]::ExtractToDirectory("Winscp-automation.zip", "Winscp-automation\")
+  Add-Type -Path 'Winscp-automation\WinSCPnet.dll'
 
   # do the upload
   $sessionOptions = New-Object WinSCP.SessionOptions -Property @{
